aed46a0529db9d86bb0810585ef5f2841c6cb113
Antonio-Alisson-Teixeira-Cruz <ravenjouki@gmail.com>
2025-03-16 16:45:10 -0300

    Add files via upload

---
+++ src/js/script.js
@@ -0,0 +1,29 @@
+const form = document.getElementById('devForm')
+form.addEventListener('submit', function(noDefault) {
+    noDefault.preventDefault()
+})
+
+const techList = document.getElementById('technologyList')
+const btnNewTechnology = document.getElementById('newTechnology')
+
+function newRow() {
+    const div = document.createElement('div')
+    div.innerHTML = `<label for="technology">Nova Tecnologia</label><br>
+    <input type="text" name="technology"><br><br>
+    
+    <label for="experience">Tempo de experiência</label><br>
+    <input type="radio" name="experienceTime" value="0-2 anos">
+    <input type="radio" name="experienceTime" value="3-4 anos">
+    <input type="radio" name="experienceTime" value="5+ anos"><br><br>
+
+    <button class="removeTechnology" type="button">Remover tecnologia</button>`
+
+    techList.appendChild(div)
+
+    const removeButton = div.querySelector('.removeTechnology')
+    removeButton.addEventListener('click', function() {
+        techList.removeChild(div)
+    })
+}
+
+btnNewTechnology.addEventListener('click', newRow)
