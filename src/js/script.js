d14e3da47e95627f7fb273d76b96435cd40cd681
Antonio-Alisson-Teixeira-Cruz <ravenjouki@gmail.com>
2025-03-16 17:00:17 -0300

    Update script.js

--- src/js/script.js
+++ src/js/script.js
@@ -10,15 +10,15 @@ function newRow() {
     const div = document.createElement('div')
     div.innerHTML = `<label for="technology">Nova Tecnologia</label><br>
     <input type="text" name="technology"><br><br>
-    
-    <label for="experience">Tempo de experiência</label><br>
-    <label for="zeroTwoYears">0-2 anos</label>
-    <input type="radio" name="experienceTime" value="0-2 anos" id="zeroTwoYears">
-    <label for="threeFourYears">3-4 anos</label>
-    <input type="radio" name="experienceTime" value="3-4 anos" id="threeFourYears">
-    <label for="fiveOrMoreYears">5+ anos</label>
-    <input type="radio" name="experienceTime" value="5+ anos" id="fiveOrMoreYears"><br><br>
 
+    <p>Nova tecnologia</p>
+    <input type="radio" id="zeroTwo" name="experience" value="0-2 anos">
+    <label for="zeroTwo">0-2 anos</label><br>
+    <input type="radio" id="threeFour" name="experience" value="3-4 anos">
+    <label for="threeFour">3-4 anos</label><br>
+    <input type="radio" id="fiveMore" name="experience" value="5+ anos">
+    <label for="fiveMore">5+anos</label>
+    
     <button class="removeTechnology" type="button">Remover tecnologia</button>`
 
     techList.appendChild(div)
